f8b514d0479dcb634a301870c647b6dd0fc9777d
Ari Lamstein <arilamstein@gmail.com>
2026-03-18 12:11:14 -0700

    Support for viewing state and county data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,33 @@ import streamlit as st
 import pandas as pd
 import acs_nativity
 
+# Load all data
 df_us = pd.read_csv("us.csv")
 df_state = pd.read_csv("state.csv")
 df_county = pd.read_csv("county.csv")
 
+# Let use choose what to see
 geography = st.selectbox(label="Geography:", options=["Nation", "State", "County"])
+if geography == "Nation":
+    df = df_us
+elif geography == "State":
+    df = df_state.copy()
+    state = st.selectbox(label="State:", options=df["Name"].unique())
+    df = df[df["Name"] == state]
+
+elif geography == "County":
+    df = df_county.copy()
+    state = st.selectbox(label="State:", options=df["State"].unique())
+    counties = df[df["State"] == state]["County"]
+    county = st.selectbox(label="County:", options=counties.unique())
+    df = df[(df["State"] == state) & (df["County"] == county)]
+else:
+    raise ValueError("Unknown geography")
+
 column = st.selectbox(
     "Demographic:", options=["Foreign-born", "Percent Foreign-born", "Native", "Total"]
 )
 
-st.plotly_chart(acs_nativity.plot_nativity_timeseries(df_us, column))
-st.plotly_chart(acs_nativity.plot_nativity_change(df_us, column))
+# Make charts
+st.plotly_chart(acs_nativity.plot_nativity_timeseries(df, column))
+st.plotly_chart(acs_nativity.plot_nativity_change(df, column))
