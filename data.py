fccee31602e1c84fb0f30dfc21a08da6cce3c7b3
Ari Lamstein <arilamstein@gmail.com>
2026-03-18 14:09:51 -0700

    Add type annotations
    Also run mypy as park of CI commit workflow.

--- data.py
+++ data.py
@@ -5,23 +5,23 @@ df_state = pd.read_csv("state.csv")
 df_county = pd.read_csv("county.csv")
 
 
-def get_nation_data():
+def get_nation_data() -> pd.DataFrame:
     return df_nation.copy()
 
 
-def get_state_names():
+def get_state_names() -> list[str]:
     return df_state["Name"].unique().tolist()
 
 
-def get_state_data(state):
+def get_state_data(state: str) -> pd.DataFrame:
     return df_state[df_state["Name"] == state].copy()
 
 
-def get_county_names(state):
+def get_county_names(state: str) -> list[str]:
     return df_county[df_county["State"] == state]["County"].unique().tolist()
 
 
-def get_county_data(state, county):
+def get_county_data(state: str, county: str) -> pd.DataFrame:
     return df_county[
         (df_county["State"] == state) & (df_county["County"] == county)
     ].copy()
